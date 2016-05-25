c90ca659eaa201ff990ea1e371ce7873ef7ea658
Giovanni Rodighiero <giova.rod93@gmail.com>
2016-05-25 14:54:32 +0200

    update angular

--- app/assets/frontend/javascript/company/company.js
+++ app/assets/frontend/javascript/company/company.js
@@ -3,10 +3,10 @@
   var umap = angular.module('umap.company',['ui.router','ngResource']);
   umap.config(['$stateProvider','$urlRouterProvider','$locationProvider',function($stateProvider, $urlRouterProvider,$locationProvider){
     $stateProvider.state('addCompanies', {
-      url: '/superAdmin/addCompanies',
+      url: '/superAdmin/addCompany',
       views: {
             'content': {
-              templateUrl: 'assets/html/superAdmin/companies.html',
+              templateUrl: 'assets/html/superAdmin/companies/addCompany.html',
               controller:  'CompanyController'
             }
         }
@@ -44,22 +44,17 @@ $stateProvider.state('updateCompany', {
     });
 
 
-  umap.controller('CompanyController',['$scope','CompanyService','$http','$stateParams', function($scope, CompanyService,$http, $stateParams) {
+  umap.controller('CompanyController',['$scope','CompanyService','$http','$stateParams','$location', function($scope, CompanyService,$http, $stateParams,$location) {
      $scope.company = {'companyName':''};
       $scope.addCompany = function(){
         CompanyService.save($scope.company, function(){
-
+          $location.path('/superAdmin/companies')
         });
       };
-    $scope.addTest = function(){
-      $http.post('/api/companies', $scope.company).success(function(){
-        console.log('ok');
-      });
-    }
     $scope.companies = CompanyService.query();
     $scope.deleteCompany = function(param){
       CompanyService.delete({id:  param}, function(){
-
+        $location.path('/superAdmin/companies')
       });
     };
   }]);
@@ -68,7 +63,7 @@ $stateProvider.state('updateCompany', {
     $scope.company = CompanyService.get({ id:  $stateParams.id });
     $scope.editCompany = function(){
       CompanyService.update({id:  $stateParams.id}, $scope.company, function(){
-
+        $location.path('/superAdmin/companies')
       });
     }
   }]);
