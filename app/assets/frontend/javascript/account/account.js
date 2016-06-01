822ca1a600b6704624ff458b8196dea8de7807c6
Giovanni Rodighiero <giova.rod93@gmail.com>
2016-06-01 23:09:15 +0200

    added change account info superAdmin

--- app/assets/frontend/javascript/account/account.js
+++ app/assets/frontend/javascript/account/account.js
@@ -1,9 +1,32 @@
 (function(){
   var umap = angular.module('umap.account',['ui.router','ngResource']);
-
+  umap.config(['$stateProvider','$urlRouterProvider','$locationProvider',function($stateProvider, $urlRouterProvider,$locationProvider){
+    $stateProvider.state('root.account', {
+      url: 'account',
+      views: {
+            'content@': {
+              templateUrl: 'assets/html/shared/account.html',
+              controller:  'AccountController'
+            }
+        }
+  });
+}]);
   umap.factory('AccountService', function($resource) {
     return $resource('/api/account', {}, {
-      query: {method:'GET', isArray:false}
+      update: {method:'PUT'}
     })
   });
+  umap.controller('AccountController',['AccountService','CompanyService','$scope','$state',function(AccountService,CompanyService,$scope,$state){
+    AccountService.get().$promise.then(function(account){
+      CompanyService.get({id: account.company}).$promise.then(function(company){
+        $scope.account = account;
+        $scope.company = company.companyName;
+      });
+    });
+    $scope.editUser = function(){
+      AccountService.update({}, $scope.account, function(){
+        $state.go('root')
+      });
+    }
+  }]);
 })();
--- app/controllers/shared/account/AccountController.scala
+++ app/controllers/shared/account/AccountController.scala
@@ -1,13 +1,22 @@
 package controllers.shared.account
 
+
 import java.util.UUID
 import javax.inject.Inject
 
-import com.mohiva.play.silhouette.api.{ Environment, LogoutEvent, Silhouette }
+import com.mohiva.play.silhouette.api._
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.services.AvatarService
+import com.mohiva.play.silhouette.api.util.PasswordHasher
 import com.mohiva.play.silhouette.impl.authenticators.JWTAuthenticator
-//import forms.formAccount._
-import models._
-import play.api.i18n.MessagesApi
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import forms.user._
+import models.User
+import models.services._
+import models.daos.user._
+import models.daos.password._
+import models.daos.company._
+import play.api.i18n.{ MessagesApi, Messages }
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.Json
 import play.api.mvc.Action
@@ -16,6 +25,9 @@ import scala.concurrent.Future
 
 class AccountController @Inject() (
   val messagesApi: MessagesApi,
+  userDao: UserDAO,
+  companyDao: CompanyDAO,
+  passwordInfoDao: PasswordInfoDAO,
   val env: Environment[User, JWTAuthenticator])
   extends Silhouette[User, JWTAuthenticator] {
 
@@ -23,6 +35,48 @@ class AccountController @Inject() (
 
    def getInfo = SecuredAction.async { implicit request =>
      Future.successful(Ok(Json.toJson(request.identity)))
+   }
+
+   def updateAccount = SecuredAction.async(parse.json) { implicit request =>
+       request.body.validate[EditUser.Data].map { data =>
+         userDao.findByID(request.identity.userID).flatMap {
+           case None => Future.successful(BadRequest(Json.obj("message" -> Messages("user.notComplete"))))
+           case Some(user) =>
+           val loginInfo = LoginInfo(CredentialsProvider.ID, user.email)
+           val loginInfoNew = LoginInfo(CredentialsProvider.ID, data.email)
+           passwordInfoDao.find(loginInfo).flatMap{
+             case None =>
+               Future.successful(BadRequest(Json.obj("message" -> Messages("mail.notExists"))))
+             case Some(psw) =>
+               val authInfo = psw
+               for{
+                 authInfo <- passwordInfoDao.updateNewLoginInfo(loginInfo, loginInfoNew, authInfo)
+               }yield {
+                 Ok(Json.obj("token" -> "ok"))
+                }
+           }
+             val companyInfo = companyDao.findByIDUser(request.identity.userID)
+             val user2 = User(
+               userID = user.userID,
+               name = data.name,
+               surname = data.surname,
+               loginInfo = loginInfoNew,
+               email = data.email,
+               company = user.company,
+               role = user.role
+             )
+             for {
+               user <- userDao.update(request.identity.userID,user2)
+               authenticator <- env.authenticatorService.create(loginInfo)
+               //token <- env.authenticatorService.init(authenticator)
+             } yield {
+               Ok(Json.obj("token" -> "ok"))
+             }
+         }
+       }.recoverTotal {
+         case error =>
+           Future.successful(Unauthorized(Json.obj("message" -> Messages("invalid.data"))))
+     }
    }
   //
   //  def editMyCompany = SecuredAction.async { implicit request =>
