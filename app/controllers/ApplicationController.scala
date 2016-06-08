ad4d2bdae25f2a5be36952452b4feb35f2332a5d
Marco Baggio <marcobgg4@gmail.com>
2016-06-08 11:38:31 +0200

    identazione

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -36,54 +36,53 @@ class ApplicationController @Inject() (
   def user = SecuredAction.async { implicit request =>
     Future.successful(Ok(Json.toJson(request.identity)))
   }
-def test = UserAwareAction.async { implicit request =>
+  def test = UserAwareAction.async { implicit request =>
   Future.successful(Ok(Json.obj("test"->"test")))
-}
-def correlation = Action.async { implicit request =>
-  val a: List[Double] = List(1.2, 2.1, 3.2, 3, 3, 3)
-  val b: List[Double] = List(1.2, 2.1, 3.2, 3, 3, 3)
-  val e = new Engine
-  val aux: Double = e.getCorrelation(a,b)
-  Future.successful(Ok(Json.obj("coeff"->aux)))
-}
-// SUMSTATISTIC FUNZIONANTE
+  }
+  def correlation = Action.async { implicit request =>
+    val a: List[Double] = List(1.2, 2.1, 3.2, 3, 3, 3)
+    val b: List[Double] = List(1.2, 2.1, 3.2, 3, 3, 3)
+    val e = new Engine
+    val aux: Double = e.getCorrelation(a,b)
+    Future.successful(Ok(Json.obj("coeff"->aux)))
+  }
+  // SUMSTATISTIC
+  def sumStatistic(mv: String) = Action.async { implicit request =>
+    val obs: Array[Double] = Array(1.2, 2, 3, 3, 3, 3)
+    val obs2: Array[Double] = Array(0, 1, 0, 3, 3, 3)
+    val lista: List[Array[Double]] =List(obs,obs2)
+    val e = new Engine
+    val aux: Array[Double] = e.sumStatistic(lista, mv)
+    Future.successful(Ok(Json.obj("Array"->aux)))
+  }
 
-def sumStatistic(mv: String) = Action.async { implicit request =>
-  val obs: Array[Double] = Array(1.2, 2, 3, 3, 3, 3)
-  val obs2: Array[Double] = Array(0, 1, 0, 3, 3, 3)
-  val lista: List[Array[Double]] =List(obs,obs2)
-  val e = new Engine
-  val aux: Array[Double] = e.sumStatistic(lista, mv)
-  Future.successful(Ok(Json.obj("Array"->aux)))
-}
+  def NaiveBayes = Action.async { implicit request =>
+    val obs: Array[Double] = Array(1.2, 2, 3)
+    val obs2: Array[Double] = Array(0, 1, 0)
+    val obs3: Array[Double] = Array(1.2,2,3)
+    val health: List[Double]= List(0.0,1.0,0.0)
+    val lista: List[Array[Double]] = List(obs,obs2,obs3)
+    val e = new Engine
+    val aux:NaiveBayesModel = e.createModel(health,lista)
+    val temp:Array[Double] = e.prediction(lista,aux)
+    Future.successful(Ok(Json.obj("Array"->temp)))
+  }
 
-def NaiveBayes = Action.async { implicit request =>
-  val obs: Array[Double] = Array(1.2, 2, 3)
-  val obs2: Array[Double] = Array(0, 1, 0)
-  val obs3: Array[Double] = Array(1.2,2,3)
-  val health: List[Double]= List(0.0,1.0,0.0)
-  val lista: List[Array[Double]] = List(obs,obs2,obs3)
-  val e = new Engine
-  val aux:NaiveBayesModel = e.createModel(health,lista)
-  val temp:Array[Double] = e.prediction(lista,aux)
-  Future.successful(Ok(Json.obj("Array"->temp)))
-}
+  def LogReg = Action.async { implicit request =>
+    val obs: Array[Double] = Array(1.2, 2, 3)
+    val obs2: Array[Double] = Array(0, 1, 0)
+    val obs3: Array[Double] = Array(1.2,2,3)
+    val health: List[Double]= List(0.0,1.0,0.0)
+    val lista: List[Array[Double]] = List(obs,obs2,obs3)
+    val e = new Engine
+    val modello:LogRegModel = e.getLogRegModel(health,lista)
+    val predizione:Array[Double] = e.getLogRegPrediction(modello,lista)
+    Future.successful(Ok(Json.obj("Array"->predizione)))
+  }
 
-def LogReg = Action.async { implicit request =>
-  val obs: Array[Double] = Array(1.2, 2, 3)
-  val obs2: Array[Double] = Array(0, 1, 0)
-  val obs3: Array[Double] = Array(1.2,2,3)
-  val health: List[Double]= List(0.0,1.0,0.0)
-  val lista: List[Array[Double]] = List(obs,obs2,obs3)
-  val e = new Engine
-  val modello:LogRegModel = e.getLogRegModel(health,lista)
-  val predizione:Array[Double] = e.getLogRegPrediction(modello,lista)
-  Future.successful(Ok(Json.obj("Array"->predizione)))
-}
-
-def index = UserAwareAction.async { implicit request =>
-  Future.successful(Ok(Json.obj("test"->"contenuto")))
-}
+  def index = UserAwareAction.async { implicit request =>
+    Future.successful(Ok(Json.obj("test"->"contenuto")))
+  }
   /**
    * Manages the sign out action.
    */
