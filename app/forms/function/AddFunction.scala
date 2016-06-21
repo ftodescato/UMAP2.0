ef4d5fbaceb5ec027a594f6afedbb26f8ff8b5c1
ftodescato <tode.92@hotmail.it>
2016-06-21 10:51:32 +0200

    add form function

---
+++ app/forms/function/AddFunction.scala
@@ -0,0 +1,35 @@
+package forms.function
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.libs.json.Json
+
+
+object AddFunction {
+
+  /**
+   * A play framework form.
+   */
+  val form = Form(
+    mapping(
+      "functionName" -> nonEmptyText
+    )(Data.apply)(Data.unapply)
+  )
+
+
+  case class Data(
+    functionName: String
+    )
+
+
+  /**
+   * The companion object.
+   */
+  object Data {
+
+    /**
+     * Converts the [Date] object to Json and vice versa.
+     */
+    implicit val jsonFormat = Json.format[Data]
+  }
+}
