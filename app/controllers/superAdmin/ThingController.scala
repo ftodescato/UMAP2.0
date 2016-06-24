d8cc7ddfa6435a2219c48499fced906ed8f0f6b2
ftodescato <tode.92@hotmail.it>
2016-06-24 11:32:18 +0200

    aggiunta form selezionaFunzioni admin e relativo metodo in nuovo controller

--- app/controllers/superAdmin/ThingController.scala
+++ app/controllers/superAdmin/ThingController.scala
@@ -14,20 +14,19 @@ import models.Measurements
 import models.daos.company.CompanyDAO
 import models.daos.thingType.ThingTypeDAO
 import models.daos.thing.ThingDAO
-// import models.daos.measurements.MeasurementsDAO
 import play.api.i18n.{ MessagesApi, Messages }
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.Json
 import play.api.mvc.Action
-import scala.collection.mutable.ListBuffer
 
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Future
 
 //import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
 //import com.mohiva.play.silhouette.api.services.AvatarService
 //import com.mohiva.play.silhouette.api.util.PasswordHasher
 //import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
 
-import scala.concurrent.Future
 
 
 class ThingController @Inject() (
@@ -38,7 +37,6 @@ class ThingController @Inject() (
   val env: Environment[User, JWTAuthenticator],
   thingDao: ThingDAO,
   thingTypeDao: ThingTypeDAO,
-  // measurementsDao: MeasurementsDAO,
   companyDao: CompanyDAO)
 extends Silhouette[User, JWTAuthenticator] {
 
@@ -74,17 +72,6 @@ extends Silhouette[User, JWTAuthenticator] {
     request.body.validate[EditThing.Data].map { data =>
           thingDao.findByID(thingID).flatMap{
             case Some(thingTypeToAssign) =>
-              // var detectionInitial = Detection(
-              //     sensor = "",
-              //     value = 0
-              // )
-              // var listDetectionInitial = List(detectionInitial)
-              // var datasInitial = Measurements(
-              //   dataTime = null,
-              //   sensors = listDetectionInitial,
-              //   healty = false
-              // )
-              // var listMeasurements = List(datasInitial)
               val thing2 = Thing(
                   thingID = thingTypeToAssign.thingID,
                   name = data.name,
