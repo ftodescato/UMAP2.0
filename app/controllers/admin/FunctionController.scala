3bace6c94271165de362a11c6f39e28f7076dc3e
Giovanni Rodighiero <giova.rod93@gmail.com>
2016-07-08 00:49:15 +0200

    fixed

--- app/controllers/admin/FunctionController.scala
+++ app/controllers/admin/FunctionController.scala
@@ -40,6 +40,8 @@ class FunctionController @Inject() (
   *   metodo che permette agli admin di selezionare le funzioni
   *   da rendere disponibile all'utente nel calcolo dei valori dei grafici
   */
+
+
   def selectFunction = SecuredAction(WithServices(Array("admin"), true)).async(parse.json){ implicit request =>
     // richiesta alla form forms.engine.SelectFunctionAdmin
     request.body.validate[SelectFunctionAdmin.Data].map { data =>
